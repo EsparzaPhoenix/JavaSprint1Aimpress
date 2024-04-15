6476528ec2fd5ba580c53d76f0819de8e88daa87
EsparzaPhoenix <esparza_leonardo@hotmail.com>
2024-04-15 00:26:07 -0300

    CurriculoController Funcional Adicionado

--- src/main/java/br/com/fiap/JavaSprint1Aimpress/controller/CurriculoController.java
+++ src/main/java/br/com/fiap/JavaSprint1Aimpress/controller/CurriculoController.java
@@ -12,21 +12,48 @@ import org.springframework.web.util.UriComponentsBuilder;
 import br.com.fiap.JavaSprint1Aimpress.repository .CurriculoRepository;
 
 @RestController
-@RequestMapping("/curriculo")
+@RequestMapping("/curriculos") // Adicionando o mapeamento para a URL base "/curriculos"
 public class CurriculoController {
 
     @Autowired
-    private CurriculoRepository curriculoRepository;
+    public CurriculoRepository curriculoRepository;
 
-    @PostMapping
+    @PostMapping("/cadastrar") // Mapeamento para a URL "/curriculos/cadastrar"
     @Transactional
-    public ResponseEntity<DetalhesCurriculoDto> cadastrar(@RequestBody @Valid CadastrarCurriculoDto dto, UriComponentsBuilder uriBuilder){
+    public ResponseEntity<DetalhesCurriculoDto> cadastrar(@RequestBody @Valid CadastrarCurriculoDto dto, UriComponentsBuilder uriBuilder) {
+        if (!dto.Valido()) {
+            return ResponseEntity.badRequest().build();
+        }
 
         var curriculo = new Curriculo(dto);
         curriculoRepository.save(curriculo);
-        var url = uriBuilder.path("curriculos/{id}").buildAndExpand(curriculo.getId()).toUri();
+        var url = uriBuilder.path("/curriculos/{id}").buildAndExpand(curriculo.getId()).toUri();
         return ResponseEntity.created(url).body(new DetalhesCurriculoDto(curriculo));
-
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<DetalhesCurriculoDto> getById(@PathVariable Long id) {
+        var curriculo = curriculoRepository.findById(id);
+        return curriculo.map(value -> ResponseEntity.ok(new DetalhesCurriculoDto(value))).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
+
+//@RestController
+//@RequestMapping("/curriculo")
+//public class CurriculoController {
+//
+//    @Autowired
+//    private CurriculoRepository curriculoRepository;
+//
+//    @PostMapping
+//    @Transactional
+//    public ResponseEntity<DetalhesCurriculoDto> cadastrar(@RequestBody @Valid CadastrarCurriculoDto dto, UriComponentsBuilder uriBuilder){
+//
+//        var curriculo = new Curriculo(dto);
+//        curriculoRepository.save(curriculo);
+//        var url = uriBuilder.path("curriculos/{id}").buildAndExpand(curriculo.getId()).toUri();
+//        return ResponseEntity.created(url).body(new DetalhesCurriculoDto(curriculo));
+//
+//    }
+//
+//}
--- src/main/java/br/com/fiap/JavaSprint1Aimpress/dto/CadastrarCurriculoDto.java
+++ src/main/java/br/com/fiap/JavaSprint1Aimpress/dto/CadastrarCurriculoDto.java
@@ -8,12 +8,19 @@ public record CadastrarCurriculoDto(
         @NotBlank
         String nome,
         @NotBlank
-        @Size(max = 11, min =11, message = "Numero de Telefone deve conter 11 caracteres ex: 11943215432")//numero de telefone com ddd
+        @Size(max = 11, min =11, message = "Numero de Telefone deve conter 11 caracteres ex: 11943215432")
         String numeroTelefone,
-        @NotBlank
         NivelEscolaridade nivelEscolaridade,
         @NotBlank
         String habilidades,
         @NotBlank
-        String certificacoes) {
+        String certificacoes
+) {
+        public boolean Valido() {
+                return nome() != null &&
+                        numeroTelefone() != null &&
+                        nivelEscolaridade() != null &&
+                        habilidades() != null &&
+                        certificacoes() != null;
+        }
 }
